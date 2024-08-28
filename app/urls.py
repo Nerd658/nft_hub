4eb5c052a3b98d0de209aecb8f3dfdda3301e30b
M Hassan BIKIENGA <m9bikienga@gmail.com>
2024-08-28 13:52:12 +0000

    Ajout de nouvelles fonctionnalités

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('acceuil', views.acceuil , name="acceuil"),
+    path('', views.acceuil , name="acceuil"),
+    path('contact', views.contact , name="contact"),
+    path('connexion', views.connexion , name="connexion"),
+    
     
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
-
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.http import HttpResponse
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+import re
+from django.db.models import Q
 def acceuil (request) :
     return render(request, 'app/acceuil.html')
 # Create your views here.
+
+def contact (request) :
+    return render (request, 'app/contact.html')
+
+def connexion (request) :
+    return render (request, 'compte/connexion.html')
